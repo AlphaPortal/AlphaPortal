eaf80358dba0fe00c21d84336c7b48f7349a120d
Björn Åhström <bjorn.ahstrom@icloud.com>
2025-03-27 19:24:48 +0100

    Dashboard controller created

---
+++ Presentation/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+
+    public class DashboardController : Controller
+    {
+        [Route("admin/dashboard")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

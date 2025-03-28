bac5d37cd01dd73bda68f14f59b214da38a56c6a
Björn Åhström <bjorn.ahstrom@icloud.com>
2025-03-28 16:18:08 +0100

    AuthController created

---
+++ Presentation/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using Authentication.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class AuthController(IAuthService authService) : Controller
+    {
+
+        private readonly IAuthService _authService = authService;
+
+        [Route("auth/signin")]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("auth/signin")]
+        public IActionResult SignIn()
+        {
+
+
+            return View();
+        }
+
+        [Route("auth/signup")]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [Route("auth/logout")]
+        public IActionResult LogOut()
+        {
+            return View();
+        }
+    }
+}

21b421deaa57b405ac9219c0d4fef0193f7db1d7
Björn Åhström <bjorn.ahstrom@icloud.com>
2025-03-31 15:02:50 +0200

    Notification item created and css styles added. Changed the size on the notification and settings icons.

---
+++ Business/Models/NotificationItem.cs
@@ -0,0 +1,10 @@
+﻿using System.Data;
+
+namespace Business.Models;
+
+public class NotificationItem
+{
+    public string ProjectName { get; set; } = null!;
+    public DateTime CreatedAt { get; set; }
+    public string ImageUrl { get; set; } = null!;
+}

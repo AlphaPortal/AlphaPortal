daa4be3ed88468a84d12811f4f0696c31bede70c
Björn Åhström <bjorn.ahstrom@icloud.com>
2025-04-01 06:42:02 +0200

    Projects controller created

---
+++ Presentation/Controllers/ProjectsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers;
+
+[Authorize]
+public class ProjectsController : Controller
+{
+
+    [Route("admin/projects")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}

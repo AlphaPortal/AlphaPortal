cd4418881126ab21abb42b3740fc64bd0cec2e7b
Björn Åhström <bjorn.ahstrom@icloud.com>
2025-04-02 13:48:17 +0200

    Notification entites created and implemented in DataContex

--- Data/Contexts/DataContext.cs
+++ Data/Contexts/DataContext.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Data.Contexts;
 
 public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
 {
+    public virtual DbSet<NotificationEntity> Notifications { get; set; }
+    public virtual DbSet<NotificationTargetEntity> NotificationTargets { get; set; }
+    public virtual DbSet<NotificationTypeEntity> NotificationTypes { get; set; }
+    public virtual DbSet<UserDismissNotificationEntity> UserDismissNotifications { get; set; }
 }
---
+++ Data/Entities/UserDismissNotificationEntity.cs
@@ -0,0 +1,21 @@
+﻿using Authentication.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Entities;
+
+public class UserDismissNotificationEntity
+{
+    [Key]
+    public int Id { get; set; }
+
+
+    [ForeignKey(nameof(User))]
+    public string UserId { get; set; } = null!;
+    public virtual AppUser User { get; set; } = null!;
+
+
+    [ForeignKey(nameof(Notification))]
+    public string NotificationId { get; set; } = null!;
+    public virtual NotificationEntity Notification { get; set; } = null!;
+}

bd60483efde5d73aefc7b79acb1309906c1232c2
Björn Åhström <bjorn.ahstrom@icloud.com>
2025-04-02 13:33:52 +0200

    Updated BaseRepository with Interface

---
+++ Data/Interfaces/IBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Responses;
+using System.Linq.Expressions;
+
+namespace Data.Interfaces
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        Task<RepositoryResult> AddAsync(TEntity entity);
+        Task<RepositoryResult> DeleteAsync(Expression<Func<TEntity, bool>> expression);
+        Task<RepositoryResult> ExistsAsync(Expression<Func<TEntity, bool>> expression);
+        Task<RepositoryResult<IEnumerable<TEntity>>> GetAllAsync(bool orderByDescending = false, Expression<Func<TEntity, object>>? sortByColumn = null, Expression<Func<TEntity, bool>>? filterBy = null, int take = 0, Expression<Func<TEntity, object>>[] includes = null);
+        Task<RepositoryResult<TEntity>> GetAsync(Expression<Func<TEntity, bool>> findBy, params Expression<Func<TEntity, object>>[] includes);
+        Task<RepositoryResult> UpdateAsync(TEntity entity);
+    }
+}
--- Data/Repositories/BaseRepository.cs
+++ Data/Repositories/BaseRepository.cs
@@ -1,99 +1,159 @@
 ﻿using Data.Contexts;
+using Data.Interfaces;
+using Domain.Responses;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using System.Linq.Expressions;
 
 namespace Data.Repositories;
 
-public abstract class BaseRepository<TEntity>(DataContext context) where TEntity : class
+public abstract class BaseRepository<TEntity>(DataContext context) : IBaseRepository<TEntity> where TEntity : class
 {
     protected readonly DataContext _context = context;
     protected readonly DbSet<TEntity> _db = context.Set<TEntity>();
 
-    public virtual async Task<bool> AddAsync(TEntity entity)
+    public virtual async Task<RepositoryResult> AddAsync(TEntity entity)
     {
         try
         {
+            if (entity == null)
+            {
+                return new RepositoryResult { Succeeded = false, StatusCode = 400, Error = "Invalid properties." };
+            }
             await _db.AddAsync(entity);
             await _context.SaveChangesAsync();
+            return new RepositoryResult { Succeeded = true, StatusCode = 202 };
 
-            return true;
-        } catch (Exception ex)
+        }
+        catch (Exception ex)
         {
             Debug.WriteLine(ex.Message);
-            return false;
+            return new RepositoryResult { Succeeded = false, StatusCode = 500, Error = ex.Message };
         }
     }
 
-    public virtual async Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> expression)
+    public virtual async Task<RepositoryResult<IEnumerable<TEntity>>> GetAllAsync(bool orderByDescending = false, Expression<Func<TEntity, object>>? sortByColumn = null, Expression<Func<TEntity, bool>>? filterBy = null, int take = 0, Expression<Func<TEntity, object>>[] includes)
     {
-        try
-        {
-            var result = await _db.AnyAsync(expression);
-            return result;
+        IQueryable<TEntity> query = _db;
 
-        } catch (Exception ex)
+        if (filterBy != null)
         {
-            Debug.WriteLine(ex.Message);
-            return false;
+            query = query.Where(filterBy);
         }
+
+        if (includes != null && includes.Length != 0)
+        {
+            foreach (var item in includes)
+            {
+                query = query.Include(item);
+            }
+        }
+
+        if (sortByColumn != null)
+        {
+            query = orderByDescending ? query.OrderByDescending(sortByColumn) : query.OrderBy(sortByColumn);
+        }
+
+        if (take > 0)
+        {
+            query.Take(take);
+        }
+
+        var entities = await query.ToListAsync();
+        return new RepositoryResult<IEnumerable<TEntity>> { Succeeded = true, StatusCode = 200, Result = entities };
+
     }
 
-    public virtual async Task<IEnumerable<TEntity>> GetAllAsync()
+    public virtual async Task<RepositoryResult<TEntity>> GetAsync(Expression<Func<TEntity, bool>> findBy, params Expression<Func<TEntity, object>>[] includes)
     {
-        try
-        {
-            var entites = await _db.ToListAsync();
-            return entites;
+        IQueryable<TEntity> query = _db;
 
-        } catch (Exception ex)
+        if (findBy == null)
         {
-            Debug.WriteLine(ex.Message);
-            return null!;
+            return new RepositoryResult<TEntity> { Succeeded = false, StatusCode = 400, Error = "Expression not defined." };
         }
+
+        if (includes != null && includes.Length != 0)
+        {
+            foreach (var item in includes)
+            {
+                query = query.Include(item);
+            }
+        }
+
+        var entity = await query.FirstOrDefaultAsync(findBy);
+        return entity != default
+            ? new RepositoryResult<TEntity> { Succeeded = true, StatusCode = 200, Result = entity }
+            : new RepositoryResult<TEntity> { Succeeded = false, StatusCode = 404, Error = "Entity not found." };
     }
 
-    public virtual async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> expression)
+    public virtual async Task<RepositoryResult> UpdateAsync(TEntity entity)
     {
         try
         {
-            var entity = await _db.FirstOrDefaultAsync(expression);
-            return entity ?? null!; 
+            if (entity == null)
+            {
+                return new RepositoryResult { Succeeded = false, StatusCode = 400, Error = "Invalid properties." };
+            }
 
-        } catch (Exception ex)
-        {
-            Debug.WriteLine(ex.Message);
-            return null!;
-        }
-    }
+            var result = await _db.ContainsAsync(entity);
+            if (!result)
+            {
+                return new RepositoryResult { Succeeded = false, StatusCode = 404, Error = "Entity no found" };
+            }
 
-    public virtual async Task<bool> UpdateAsync(TEntity entity)
-    {
-        try
-        {
             _db.Update(entity);
             await _context.SaveChangesAsync();
-            return true;
+            return new RepositoryResult { Succeeded = true, StatusCode = 200 };
 
-        } catch (Exception ex)
+        }
+        catch (Exception ex)
         {
             Debug.WriteLine(ex.Message);
-            return false;
+            return new RepositoryResult { Succeeded = false, StatusCode = 500, Error = ex.Message };
         }
     }
 
-    public virtual async Task<bool> RemoveAsync(TEntity entity)
+    public virtual async Task<RepositoryResult> ExistsAsync(Expression<Func<TEntity, bool>> expression)
+    {
+        if (expression == null)
+        {
+            return new RepositoryResult { Succeeded = false, StatusCode = 400, Error = "Invalid expression." };
+        }
+
+        var result = await _db.AnyAsync(expression);
+        if (!result)
+        {
+            return new RepositoryResult { Succeeded = false, StatusCode = 404, Error = "Entity not found." };
+        }
+
+        return new RepositoryResult { Succeeded = true, StatusCode = 200, Error = "Entity exists" };
+    }
+
+    public virtual async Task<RepositoryResult> DeleteAsync(Expression<Func<TEntity, bool>> expression)
     {
         try
         {
+            if (expression == null)
+            {
+                return new RepositoryResult { Succeeded = false, StatusCode = 400, Error = "Invalid properties." };
+            }
+
+            var entity = await _db.FirstOrDefaultAsync(expression);
+            if (entity == null)
+            {
+                return new RepositoryResult { Succeeded = false, StatusCode = 404, Error = "Entity no found" };
+            }
+
             _db.Remove(entity);
             await _context.SaveChangesAsync();
-            return true;
+            return new RepositoryResult { Succeeded = true, StatusCode = 200 };
 
-        } catch (Exception ex)
+        }
+        catch (Exception ex)
         {
             Debug.WriteLine(ex.Message);
-            return false;
+            return new RepositoryResult { Succeeded = false, StatusCode = 500, Error = ex.Message };
         }
     }
 }

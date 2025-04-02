03c493f2658b12f6b695e529c896b76899c0136e
Björn Åhström <bjorn.ahstrom@icloud.com>
2025-04-02 13:51:46 +0200

    AddRepositories extenson created

---
+++ Data/Extensions/RepositoryExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Data.Extensions;
+
+public static class RepositoryExtensions
+{
+    public static IServiceCollection AddRepositories(this IServiceCollection services, IConfiguration configuration)
+    {
+
+
+        return services;
+    }
+
+}

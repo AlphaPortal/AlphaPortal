bf5d7e25f5fb2a29078c0e96e4f286f8848cc8cb
Björn Åhström <bjorn.ahstrom@icloud.com>
2025-04-07 18:10:34 +0200

    Implement user search and tagging functionality
    Updated MembersController to add user search feature with JSON results. Modified _AddProjectModal to include user search input and hidden user ID storage. Added tags.js for managing user tagging interactions and updated _Layout to include the new script.

--- Presentation/Controllers/MembersController.cs
+++ Presentation/Controllers/MembersController.cs
@@ -1,15 +1,43 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Authentication.Contexts;
+using Business.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Presentation.Controllers;
 
 [Authorize]
-[Route("admin/[controller]")]
 public class MembersController : Controller
 {
-    [HttpGet("")]
+    private readonly IUserService _userService;
+    private readonly AuthenticationContext _context;
+
+    public MembersController(IUserService userService, AuthenticationContext authenticationContext)
+    {
+        _userService = userService;
+        _context = authenticationContext;
+    }
+
+    [Route("admin/[controller]")]
     public IActionResult Index()
     {
         return View();
+    }
+
+    [HttpGet]
+    public async Task<JsonResult> SearchUsers(string term)
+    {
+        if(string.IsNullOrWhiteSpace(term))
+        {
+            return Json(new List<object>());
+        }
+
+        var users = await _context.Users
+            .Include(u => u.Profile)
+            .Where(u => u.Profile!.FirstName!.Contains(term) || u.Profile.LastName!.Contains(term) || u.Email!.Contains(term))
+            .Select(u => new {u.Id, u.Profile!.ImageUrl, FullName = $"{u.Profile.FirstName} {u.Profile.LastName}"})
+            .ToListAsync();
+
+        return Json(users);
     }
 }

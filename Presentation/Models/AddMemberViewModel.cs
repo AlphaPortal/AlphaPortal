6b12b3d7defe8a8dab2e6f246dfcc670bfe47617
Björn Åhström <bjorn.ahstrom@icloud.com>
2025-04-07 13:33:29 +0200

    Add member management features and UI updates
    - Updated `Presentation.csproj` to include a folder for member images.
    - Modified `Index.cshtml` to fetch users asynchronously and display them.
    - Added a using directive for `Authentication.Models` in `_ViewImports.cshtml`.
    - Enhanced styles in `site.css` for member card layout and design.
    - Created `AddMemberViewModel` for handling member data input.
    - Introduced `_MemberCard.cshtml` for rendering individual member cards.

---
+++ Presentation/Models/AddMemberViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Presentation.Models;
+
+public class AddMemberViewModel
+{
+    [DataType(DataType.Upload)]
+    [Display(Name = "Project Image", Prompt = "Select project image")]
+    public IFormFile? Image { get; set; }
+
+    [Required(ErrorMessage = "This field is required.")]
+    [DataType(DataType.Text)]
+    [Display(Name = "First Name", Prompt = "Enter first name")]
+    public string FirstName { get; set; } = null!;
+
+    [Required(ErrorMessage = "This field is required.")]
+    [DataType(DataType.Text)]
+    [Display(Name = "Last Name", Prompt = "Enter last name")]
+    public string LastName { get; set; } = null!;
+
+    [Required(ErrorMessage = "This field is required.")]
+    [DataType(DataType.EmailAddress)]
+    [Display(Name = "Email", Prompt = "Enter email address")]
+    public string Email { get; set; } = null!;
+
+    [DataType(DataType.PhoneNumber)]
+    [Display(Name = "Phone", Prompt = "Enter phone number")]
+    public string? PhoneNumber { get; set; }
+
+    [DataType(DataType.Text)]
+    [Display(Name = "Job Title", Prompt = "Enter job title")]
+    public string? JobTitle { get; set; }
+}
